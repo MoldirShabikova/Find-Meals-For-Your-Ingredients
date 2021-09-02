55e242a35785201fba769fd9bffd2ecd924cb3d0
moldir <moldirshb@gmail.com>
2021-09-01 22:50:43 -0400

    changes

--- script.js
+++ script.js
@@ -1,53 +1,56 @@
 const searchBtn = document.getElementById('search-btn');
 const foodList = document.getElementById('food');
-const foodDeskContent = document.querySelector('.food-desk-content');
-const recipeCloseBtn = document.getElementById('recipe-close-btn');
-
+let html = "";
 searchBtn.addEventListener('click', getFoodList);
-foodList.addEventListener('click', getFoodRecipe);
+
+// my API 
+
+const apiNames = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='
+// const apiRecipe = 'http://www.themealdb.com/api/json/v1/1/lookup.php?i='
 
 // getting food list 
 
-
 function getFoodList() {
-  let searchInputTxt = document.getElementById
-    ('search-input').value.trim();
-  // console.log(searchInputTxt)
-  fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+  document.querySelector(".food-wrap").style.background = "none";
+  let searchInputTxt = document.getElementById('search-input').value;
+  fetch(`${apiNames+searchInputTxt}`)
     .then(response => response.json())
     .then(data => {
       // console.log(data);
     
-               let html = "";
-                if (data.meals) {
-                data.meals.forEach(food => {
-                  html += `
+      if (data.meals) {
+        data.meals.forEach(food => {
+          html += `
                <div class = "food-item" data-id = "${food.idMeal}" >
                 <div class = "food-name">
                     <h3>${food.strMeal}</h3>
                    <div class = "food-img">
                    <img src="${food.strMealThumb}" alt="food">
                 </div>
-                   <a href = "#" class = "recipe-btn">See Recipe</a>
-                   </div>
-                      </div>
-             `;
-                })
-                  foodList.classList.remove('error');
-                } else {
-                  html = "Sorry, we didn't find any recipe!"
-                  foodList.classList.add('error')
-         }
-         foodList.innerHTML = html;
-      })  
+             <a data-id = "${food.idMeal}"href = "#" class = "recipe-btn">See Recipe</a>
+           </div>
+         </div>
+       `;
+      })
+  foodList.classList.remove('error');
+      } else {
+        html = "Sorry, we didn't find any recipe!"
+        foodList.classList.add('error')
       }
-    
-foodList.addEventListener('click', getFoodRecipe);
-      
-function getFoodRecipe(e) {
-  e.preventDefault();
-  if (e.target.classList.contains('recipe-btn')) {
-    let foodItem = e.target.parentElement.parentElement;
-    console.log(mealItem);
-  }
+      foodList.innerHTML = html;
+    })
 }
+
+// working with See Recipe btn
+
+const foodRecipe = document.getElementsByClassName('.recipe - btn');
+
+const api_url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}";
+async function getapi(url) {
+  const response = await fetch(url);
+  var data = await response.json();
+  console.log(data);
+}
+
+
+
